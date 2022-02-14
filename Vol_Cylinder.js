4f3a91c6f687fa9aadcf256b62404c15c88cd753
ABHISHEK SANGWAN <84694512+abhisheksangwan@users.noreply.github.com>
2022-02-14 15:14:09 +0530

    Added HTML , CSS, JS codes

---
+++ Vol_Cylinder.js
@@ -0,0 +1,10 @@
+// Abhishek Sangwan
+
+function getVolume(radius, height) {
+    var volume = Math.PI *radius *radius *height;
+      return volume;
+    }
+    
+    console.log(getVolume(3, 4));
+    
+    
